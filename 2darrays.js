df892b5a7180c0c22f16f9d5a9f1f4298375bf32
Michael <200~pregnant-mutt-cape@duck.com>
2025-03-17 11:53:43 -0500

    first 2d arrays

---
+++ 2darrays.js
@@ -0,0 +1,22 @@
+// 2d array each array is score for 1 student
+
+let allScores = [
+    [85, 90, 100]
+    [70, 69, 85]
+    [89, 42, 90]
+];
+
+// seating grid
+
+let theatreSeating = [
+    ["A1", "A2", "A3", "A4"]
+    ["B1", "B2", "B3", "B4"]
+    ["C1", "C2", "C3", "C4"]
+];
+
+// contacts list, Name Email and Phonenumbers
+
+let contactsList = [
+    ["Bob", "bob2@gmail.com", "789-897-2015"]
+    ["Mike", "ihatebob@gmail.com", "756-987-4581"]
+];
