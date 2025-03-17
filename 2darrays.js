175980e5192e5e52b00445628d397ff2f7967e6d
Michael <200~pregnant-mutt-cape@duck.com>
2025-03-17 12:05:56 -0500

    fixed commas

--- 2darrays.js
+++ 2darrays.js
@@ -1,22 +1,23 @@
 // 2d array each array is score for 1 student
 
 let allScores = [
-    [85, 90, 100]
-    [70, 69, 85]
+    [85, 90, 100],
+    [70, 69, 85],
     [89, 42, 90]
 ];
-
+console.log(allScores);
 // seating grid
 
 let theatreSeating = [
-    ["A1", "A2", "A3", "A4"]
-    ["B1", "B2", "B3", "B4"]
+    ["A1", "A2", "A3", "A4"],
+    ["B1", "B2", "B3", "B4"],
     ["C1", "C2", "C3", "C4"]
 ];
-
+console.log(theatreSeating);
 // contacts list, Name Email and Phonenumbers
 
 let contactsList = [
-    ["Bob", "bob2@gmail.com", "789-897-2015"]
+    ["Bob", "bob2@gmail.com", "789-897-2015"],
     ["Mike", "ihatebob@gmail.com", "756-987-4581"]
 ];
+console.log(contactsList);
